f0ed0286f382461c0c3fd8edf880012cc8cdf72c
Mlnamalik <maulanamalik_03@mhs.mdp.ac.id>
2022-10-10 11:34:23 +0700

    Initial commit

---
+++ app/src/main/java/com/if3b/maulanamalik/MainActivity.java
@@ -0,0 +1,39 @@
+package com.maulamamalik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_button;
+    TextView tvtext;
+    EditText etNama;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_button = findViewById(R.btn);
+        tvtext= findViewById(R.id.tv_text);
+        etNama = findViewById(R.id.et_nama);
+
+        btn_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Nama = etNama.getText().toString();
+
+                if (Nama.trim().equals("")) {
+                    etNama.setError("Tidak Boleh Kosong");
+                }
+                else {
+                    tvtext.setText(Nama);
+                }
+            }
+        });
+    }
+}
