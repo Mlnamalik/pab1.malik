b99c9e02dfc48b7e08860f4e04d3518279efbb2b
Mlnamalik <maulanamalik_03@mhs.mdp.ac.id>
2022-10-10 11:42:58 +0700

    up

--- app/src/main/java/com/if3b/maulanamalik/MainActivity.java
+++ app/src/main/java/com/if3b/maulanamalik/MainActivity.java
@@ -1,4 +1,4 @@
-package com.maulamamalik;
+package com.if3b.maulanamalik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_button = findViewById(R.btn);
+        btn_button = findViewById(R.id.btn_button);
         tvtext= findViewById(R.id.tv_text);
         etNama = findViewById(R.id.et_nama);
 
